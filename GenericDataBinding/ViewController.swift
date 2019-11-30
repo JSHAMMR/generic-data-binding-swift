96a6d61effa781f307f0257dcd592a714fef94d0
Jamil Shamar <jameelshammr@gmail.com>
2019-11-30 12:08:19 +0800

    init commit

---
+++ GenericDataBinding/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GenericDataBinding
+//
+//  Created by Gamil Ali Qaid Shamar on 30/11/2019.
+//  Copyright © 2019 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtFld: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func updateTxtFld(_ sender: Any) {
+        
+    }
+    
+}
+
