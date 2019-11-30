72e646569e48d1feae2d4e4228d85e0a648663f6
Jamil Shamar <jameelshammr@gmail.com>
2019-11-30 23:40:38 +0800

    add more uis to data binding

--- GenericDataBinding/DataBinding/UIControlls+Bindable.swift
+++ GenericDataBinding/DataBinding/UIControlls+Bindable.swift
@@ -31,3 +31,63 @@ extension UISwitch : Bindable {
         self.isOn = value
     }
 }
+
+extension UISlider : Bindable {
+    public typealias BindingType = Float
+    
+    public func observingValue() -> Float? {
+        return self.value
+    }
+    
+    public func updateValue(with value: Float) {
+        self.value = value
+    }
+}
+
+extension UIStepper : Bindable {
+    public typealias BindingType = Double
+    
+    public func observingValue() -> Double? {
+        return self.value
+    }
+    
+    public func updateValue(with value: Double) {
+        self.value = value
+    }
+}
+
+extension UISegmentedControl : Bindable {
+    public typealias BindingType = Int
+    
+    public func observingValue() -> Int? {
+        return self.selectedSegmentIndex
+    }
+    
+    public func updateValue(with value: Int) {
+        self.selectedSegmentIndex = value
+    }
+}
+
+public class BindableTextView: UITextView, Bindable, UITextViewDelegate {
+    public typealias BindingType = String
+    
+    public func observingValue() -> String? {
+        return self.text
+    }
+    
+    public func updateValue(with value: String) {
+        self.text = value
+    }
+    
+    public func bind(with observable: Observable<String>) {
+        self.delegate = self
+        self.register(for: observable)
+        self.observe(for: observable) { [weak self] (value) in
+            self?.updateValue(with: value)
+        }
+    }
+    
+    public func textViewDidChange(_ textView: UITextView) {
+        self.valueChanged()
+    }
+}
